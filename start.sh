20b6dfd876d3d50760c500faf15c909fecbd4757
char00les <char00les@gmail.com>
2021-09-14 15:04:02 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Start Up file
+
+#Install apache
+yum install -y httpd httpd-manual
+
+#Another Alternative way to install
+#yum groupinstall -y web-server
+
+#Start and Enable httpd
+systemctl start httpd
+systemctl enable httpd
+
+#Get local IP
+ifconfig -a 
+
+
+curl http://$IP
+
+#Install Links
+yum install -y links
+
+links http://$IP/manual
+
+systemctl status httpd
+
+#
+# Configuration
+#
+
+#main configuration directory
+ls -al /etc/httpd/
+
+#Main Configuration
+ls -al /etc/httpd/httpd.conf
+
+
+
+
